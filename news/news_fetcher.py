c50a33a47839c4a7d5df62b6f3a76fd6e53bab62
camellia-ver <jakahi435@gmail.com>
2026-05-21 11:35:19 +0900

    chore: add fetch functions

--- news/news_fetcher.py
+++ news/news_fetcher.py
@@ -8,3 +8,18 @@ class NewsFetcher:
         self._naver_client_id = NAVER_APPLICATION_CLIENT_ID
         self._naver_client_secret = NAVER_APPLICATION_CLIENT_SECRET
         self._price_checker = price_checker or PriceChecker()
+
+    def fetch_news_from_newsapi(self) -> dict:
+        '''NewsApi를 사용하여 뉴스 수집'''
+        pass
+
+    def fetch_news_from_naver(self):
+        '''Naver 검색 Api를 사용하여 뉴스 수집'''
+        pass
+
+    def get_news(self) -> dict:
+        '''수집한 뉴스들을 하나로 합쳐서 반환'''
+        newsapi_result = self.fetch_news_from_newsapi()
+        naver_result = self.fetch_news_from_naver()
+        
+        return [*newsapi_result, *naver_result]
