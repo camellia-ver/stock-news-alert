134e2b03d74288f66154e5d13867b62afc1b631a
camellia-ver <jakahi435@gmail.com>
2026-05-21 11:26:13 +0900

    refactor: update __init__ function in news_fetcher.py

--- news/news_fetcher.py
+++ news/news_fetcher.py
@@ -1,8 +1,9 @@
+from typing import Optional
 from settings import NEWS_API_KEY, NAVER_APPLICATION_CLIENT_ID, NAVER_APPLICATION_CLIENT_SECRET
 from stock.price_checker import PriceChecker
 
 class NewsFetcher:
-    def __init__(self, price_checker: PriceChecker = None) -> None:
+    def __init__(self, price_checker: Optional[PriceChecker] = None) -> None:
         self._news_api_key = NEWS_API_KEY
         self._naver_client_id = NAVER_APPLICATION_CLIENT_ID
         self._naver_client_secret = NAVER_APPLICATION_CLIENT_SECRET
